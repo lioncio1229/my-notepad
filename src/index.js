3ea5cb707c34f350f431be997bced33a1907faba
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2022-12-20 20:16:06 +0800

    Add Landing Page

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Provider from './provider';
-import Main from './routes/main';
+import LandingPage from './routes/LandingPage';
+import Main from './routes/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider>
-      <Main />
+      {/* <Main /> */}
+      <LandingPage />
     </Provider>
   </React.StrictMode>
 );
