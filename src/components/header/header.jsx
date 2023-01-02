223ed5f14cb06f336a13cb62444a170163175d80
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-01-02 11:29:38 +0800

    Implement account button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,13 +2,15 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { faFileLines } from '@fortawesome/free-solid-svg-icons';
 
-export default function Header()
+export default function Header({picture})
 {
     return (
         <div className="header flex-con">
             <h1>MY NOTEPAD</h1>
             <FontAwesomeIcon className='header-file-icon' icon={faFileLines} />
-            <FontAwesomeIcon className='user-account selectable' icon={faCircleUser} />
+            {
+                picture ? <img className='user-account selectable picture' src={picture} /> : <FontAwesomeIcon className='user-account selectable' icon={faCircleUser} />
+            }
         </div>
     );
 }
--- src/routes/main.jsx
+++ src/routes/main.jsx
@@ -9,13 +9,14 @@ import Provider from '../provider';
 export default function Main() {
 
   const MainPage = () => {
-    const storeData = useStore();
-    const {isWide, isLong, isFullscreen} = storeData.state.textEditor.display;
-  
+    const state = useStore().state;
+    const {picture} = state.global.account;
+    const {isWide, isLong, isFullscreen} = state.textEditor.display;
+
     return (
       <div className='main flex-con fcol'>
         <LoadingCircle />
-        {(!isLong && !isFullscreen) && <Header />}
+        {(!isLong && !isFullscreen) && <Header picture={picture}/>}
         <div className='my-notepad flex-con'>
           {(!isWide && !isFullscreen) && <Notes />}
           <TextEditor />
