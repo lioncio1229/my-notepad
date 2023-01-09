2ed312595598880b4c2a26a951d6d2b29264bd28
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-01-09 12:22:39 +0800

    Add useScreenWidth hook

---
+++ src/useScreenWidth.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default useScreenWidth = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return width;
+}
+
+
