ae0934467b4ca9d67c47ad4a6ad96cf24547e1fb
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-02-05 22:05:41 +0800

    Update update note api

--- src/components/notes/hooks/useUpdateNote.js
+++ src/components/notes/hooks/useUpdateNote.js
@@ -1,11 +1,13 @@
 import useStore from "../../../useStore";
-import axios from 'axios';
 import { today } from "../../../utils";
 import config from '../../../config.json';
+import useAxiosSecured from "../../../useAxiosSecured";
+import { endpoints } from "../../../config";
 
 export default function useUpdateNote()
 {
     const {dispatch, state} = useStore();
+    const axiosSecured = useAxiosSecured();
 
     const update = (note) => {
         const _note = {...note, isFresh : false, lastModified : today()};
@@ -16,9 +18,7 @@ export default function useUpdateNote()
         }
 
         dispatch({type : 'global/isLoading', payload : true});
-
-        const url = config[process.env.NODE_ENV].api.notes
-        axios({method : 'put', url, data : _note, withCredentials : true}).then(result => {
+        axiosSecured.put(endpoints.notes, _note, {withCredentials : true}).then(result => {
             if(result.status === 200)
                 dispatch({type : 'notes/update', payload : result.data});
             dispatch({type : 'global/isLoading', payload : false});
